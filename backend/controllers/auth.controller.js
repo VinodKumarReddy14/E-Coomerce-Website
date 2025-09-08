352257d7778fcb2fbe5f966e8719105e189da74f
VinodKumarReddy14 <imvinodreddy14@gmail.com>
2025-09-08 21:59:27 +0530

    feast: 2nd commit

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,7 +1,46 @@
 import User from "../models/user.model.js";
+import jwt from "jsonwebtoken";
+import { redis } from "../lib/redis.js";
+
+const generateTokens = (userid) => {
+  const accessToken = jwt.sign({ userid }, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: "15m",
+  });
+  const refreshToken = jwt.sign({ userid }, process.env.REFRESH_TOKEN_SECRET, {
+    expiresIn: "7d",
+  });
+  return { accessToken, refreshToken };
+};
+
+const saveRefreshToken = async (userid, refreshToken) => {
+  console.log(refreshToken);
+  await redis.set(
+    `refresh_token:${userid}`,
+    refreshToken,
+    "EX",
+    7 * 24 * 60 * 60 //expires in 7 days
+  );
+};
+
+const setCookies = (res, accessToken, refreshToken) => {
+  res.cookie("access-token", accessToken, {
+    httpOnly: true, //prevent xss attack- cross site script attack.
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict", //prevent csrf attack- cross site request forgery attack.
+    maxAge: 15 * 60 * 1000, //15 minutes
+  });
+
+  res.cookie("refresh-token", refreshToken, {
+    httpOnly: true, //prevent xss attack- cross site script attack.
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict", //prevent csrf attack- cross site request forgery attack.
+    maxAge: 7 * 24 * 60 * 60 * 1000, //7 days
+  });
+};
 
 export const signup = async (req, res) => {
   let { email, password, name } = req.body;
+
   try {
     let userExists = await User.findOne({ email });
     if (userExists) {
@@ -9,7 +48,21 @@ export const signup = async (req, res) => {
     }
 
     const user = await User.create({ name, email, password });
-    res.status(200).json(user, { message: "User created Successfully" });
+    const { accessToken, refreshToken } = generateTokens(user._id);
+    await saveRefreshToken(user._id, refreshToken);
+    setCookies(res, accessToken, refreshToken);
+
+    res.status(200).json(
+      {
+        user: {
+          _id: user._id,
+          name: user.name,
+          email: user.email,
+          role: user.role,
+        },
+      },
+      { message: "User created Successfully" }
+    );
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
@@ -17,4 +70,8 @@ export const signup = async (req, res) => {
 
 export const login = async (req, res) => {};
 
-export const logout = async (req, res) => {};
+export const logout = async (req, res) => {
+  try {
+    const refreshToken = req.cookie.accessToken;
+  } catch (error) {}
+};
--- backend/lib/redis.js
+++ backend/lib/redis.js
@@ -1,5 +1,8 @@
 import { Redis } from "ioredis";
+import dotenv from "dotenv";
 
-const client = new Redis(process.env.UPSTASH_REDIS_URI);
+dotenv.config();
 
-client.set("foo", "bar");
+export const redis = new Redis(process.env.UPSTASH_REDIS_URI);
+
+redis.set("foo", "bar");
